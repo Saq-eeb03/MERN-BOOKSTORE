d8068e2b6c4df39750bec72f9d59d0d780847e19
Saq-eeb03 <chmcs2026.shakeeb.chougle@gmail.com>
2026-02-11 10:25:31 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL:'http://localhost:3001'
+    //baseURL:'http://localhost:3001'
+    baseURL:'https://mern-bookstore-hy0g.onrender.com'
 })
 export default api
